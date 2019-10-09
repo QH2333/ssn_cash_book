e6cd79cd212e46acea2d261eceebd79c32e0efa5
QH2333 <qi_an_hao@126.com>
2019-10-10 00:25:17 +0800

    sahkfg

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,66 +1,38 @@
 // pages/detail/detail.js
+let app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    currentTab: 0,
+    tabArray: ["支出", "收入"]
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  //事件处理函数
+  bindChange: function (e) {
+    let that = this;
+    that.setData({
+      currentTab: e.detail.current
+    });
 
   },
+  swichNav: function (e) {
+    let that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
   },
+  onLoad: function () {
+    let that = this
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    app.getUserInfo(function (userInfo) {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+      that.setData({
+        userInfo: userInfo
+      })
+    })
   }
 })
